317e4d76221e5fd6933f62c6b1ac930f938e580e
Dorothy John <dorothyy.john@gmail.com>
2020-04-12 21:27:11 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
+
+#Return the highest integer in the array
 def find_max_value(array)
-  # Add your solution here
+    inspector = -1        #Compare the array elements with this
+ array.length.times { |i|   #iterate over the array
+   if array[i] > inspector  
+     inspector = array[i]   #Replace the larger vaues 
+   end
+ }
+ inspector
 end
